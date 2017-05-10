49f145d2dc6ed6facd5f312e9f09d3260015a412
Luke Swithenbank <lswith@users.noreply.github.com>
2017-05-10 17:50:44 +1000

    Update CRToast.h

--- CRToast/CRToast.h
+++ CRToast/CRToast.h
@@ -11,7 +11,5 @@ FOUNDATION_EXPORT double CRToastVersionNumber;
 //! Project version string for CRToast.
 FOUNDATION_EXPORT const unsigned char CRToastVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CRToast/PublicHeader.h>
-
-#import <CRToast/CRToastConfig.h>
-#import <CRToast/CRToastManager.h>
+#import "CRToastConfig.h"
+#import "CRToastManager.h"
